4ad9b03733826b65d5e4c8f6a8964ee9b4167625
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-01 23:24:29 +0900

    초기 커밋.

---
+++ day18_01/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day18_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("click");
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Mouse click");
+
+        }
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show("Mouse Down");
+
+        }
+    }
+}
